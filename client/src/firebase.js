1fb6cf33ac90740cd408eb7310005dbc9d7f4980
gautam324 <147255351+gautam324@users.noreply.github.com>
2024-11-23 00:27:24 -0800

    created listing.j

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-23f0a.firebaseapp.com",
-  projectId: "mern-estate-23f0a",
-  storageBucket: "mern-estate-23f0a.firebasestorage.app",
-  messagingSenderId: "785468147398",
-  appId: "1:785468147398:web:73b7a619a423aa55cf79c7"
+  authDomain: "test-app-cb19d.firebaseapp.com",
+  projectId: "test-app-cb19d",
+  storageBucket: "test-app-cb19d.firebasestorage.app",
+  messagingSenderId: "197549087819",
+  appId: "1:197549087819:web:d1b914764b16cd3a9081ce",
+  measurementId: "G-WV3N33HMT5"
 };
 
-// Initialize Firebase
+// // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
